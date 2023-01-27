71bb11728c82e3fb85bb050356a89c730a4f9d4a
OmriHarary <omriharary@hotmail.com>
2023-01-27 18:30:56 -0500

    Set up as Spring Boot app
    - Generated with Spring Initializer
    - Included JDBC and POI dependencies

---
+++ src/test/java/io/github/aaejo/dataimporter/DataImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aaejo.dataimporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataImporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
