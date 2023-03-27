4de62baf5e38ddf965383cfa431280955c76ea1b
OmriHarary <omriharary@hotmail.com>
2023-03-27 15:45:20 -0400

    Enable file upload over http
    - Also cleanup
    - Allow running from console in console profile

--- src/main/java/io/github/aaejo/dataimporter/DataImporter.java
+++ src/main/java/io/github/aaejo/dataimporter/DataImporter.java
@@ -1,89 +1,86 @@
 package io.github.aaejo.dataimporter;
 
-import org.apache.poi.ss.usermodel.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Optional;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
-
-import org.apache.poi.poifs.crypt.Decryptor;
-import org.apache.poi.poifs.crypt.EncryptionInfo;
-import org.apache.poi.poifs.filesystem.POIFSFileSystem;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 @Slf4j
 @Service
 public class DataImporter {
 
     private final JdbcTemplate jdbcTemplate;
+    private int count;
 
     public DataImporter(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    private static final String FILE_NAME = "C:/Users/PC/Downloads/221020_DIA_All Users.xlsx";
+    public void readFile(InputStream inStream, Optional<String> password)
+            throws IOException, EncryptedDocumentException, DataAccessException {
+        String personID, salutation, fname, mname, lname, address1, address2, address3, city, stateProv, postal,
+                countryRegion, department, institution, institutionID, primeEmail, userID, ORCID, ORCIDVal,
+                personAttribute, memberStatus;
 
-    public void readFile() throws Exception {
+        XSSFWorkbook workbook;
+        if (password.isPresent()) {
+            workbook = (XSSFWorkbook) WorkbookFactory.create(inStream, password.get());
+        } else {
+            workbook = (XSSFWorkbook) WorkbookFactory.create(inStream);
+        }
+        XSSFSheet sheet = workbook.getSheetAt(0);
 
-        String personID, salutation, fname, mname, lname, address1, address2, address3, city, stateProv, postal, countryRegion, department, institution, institutionID, primeEmail, userID, ORCID, ORCIDVal, personAttribute, memberStatus;
-
-        try {
-            FileInputStream excelFile = new FileInputStream(new File(FILE_NAME));
-            String password = "DIA_CUP";
-            XSSFWorkbook workbook = (XSSFWorkbook) WorkbookFactory.create(excelFile, password);
-            XSSFSheet sheet = workbook.getSheetAt(0);
-            Iterator<Row> iterator = sheet.iterator();
-
-            while (iterator.hasNext()) {
-                Row currentRow = iterator.next();
-                if (currentRow.getRowNum() == 0 || currentRow.getRowNum() == 1 || currentRow.getRowNum() == 2 || currentRow.getRowNum() == sheet.getLastRowNum()) {
-                    continue;
-                }
-                Iterator<Cell> cellIterator = currentRow.iterator();
-                List<String> cellValues = new ArrayList<String>();
-                while (cellIterator.hasNext()) {
-                    Cell currentCell = cellIterator.next();
-                    cellValues.add(currentCell.toString());
-                }
-                
-                personID = cellValues.get(0);
-                salutation = cellValues.get(1);
-                fname = cellValues.get(2);
-                mname = cellValues.get(3);
-                lname = cellValues.get(4);
-                address1 = cellValues.get(5);
-                address2 = cellValues.get(6);
-                address3 = cellValues.get(7);
-                city = cellValues.get(8);
-                stateProv = cellValues.get(9);
-                postal = cellValues.get(10);
-                countryRegion = cellValues.get(11);
-                department = cellValues.get(12);
-                institution = cellValues.get(13);
-                institutionID = cellValues.get(14);
-                primeEmail = cellValues.get(15);
-                userID = cellValues.get(16);
-                ORCID = cellValues.get(17);
-                ORCIDVal = cellValues.get(18);
-                personAttribute = cellValues.get(19);
-                memberStatus = cellValues.get(20);
-
-
-                jdbcTemplate.update("INSERT INTO originalfile VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", personID, salutation, fname, mname, lname, address1, address2, address3, city, stateProv, postal, countryRegion, department, institution, institutionID, primeEmail, userID, ORCID, ORCIDVal, personAttribute, memberStatus);
-
+        for (Row row : sheet) {
+            log.debug("Processing row #{}", row.getRowNum());
+            if (row.getRowNum() == 0 || row.getRowNum() == 1 || row.getRowNum() == 2
+                    || row.getRowNum() == sheet.getLastRowNum()) {
+                continue;
             }
+
+            personID = row.getCell(0).toString();
+            salutation = row.getCell(1).toString();
+            fname = row.getCell(2).toString();
+            mname = row.getCell(3).toString();
+            lname = row.getCell(4).toString();
+            address1 = row.getCell(5).toString();
+            address2 = row.getCell(6).toString();
+            address3 = row.getCell(7).toString();
+            city = row.getCell(8).toString();
+            stateProv = row.getCell(9).toString();
+            postal = row.getCell(10).toString();
+            countryRegion = row.getCell(11).toString();
+            department = row.getCell(12).toString();
+            institution = row.getCell(13).toString();
+            institutionID = row.getCell(14).toString();
+            primeEmail = row.getCell(15).toString();
+            userID = row.getCell(16).toString();
+            ORCID = row.getCell(17).toString();
+            ORCIDVal = row.getCell(18).toString();
+            personAttribute = row.getCell(19).toString();
+            memberStatus = row.getCell(20).toString();
+
+            jdbcTemplate.update(
+                    "INSERT INTO originalfile VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);",
+                    personID, salutation, fname, mname, lname, address1, address2, address3, city, stateProv,
+                    postal, countryRegion, department, institution, institutionID, primeEmail, userID, ORCID,
+                    ORCIDVal, personAttribute, memberStatus);
+
+            log.debug("Done row #{}", row.getRowNum());
+            count++;
         }
-        catch (Exception e) {
-            throw new Exception("Error reading Excel file: " + e.getMessage());
-        }
+    }
+
+    public int getCount() {
+        return count;
     }
 }
--- src/main/java/io/github/aaejo/dataimporter/DataImporterApplication.java
+++ src/main/java/io/github/aaejo/dataimporter/DataImporterApplication.java
@@ -1,28 +1,32 @@
 package io.github.aaejo.dataimporter;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class DataImporterApplication {
 
-	private final DataImporter dataImporter;
-
-	public DataImporterApplication(DataImporter dataImporter) {
-		this.dataImporter = dataImporter;
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(DataImporterApplication.class, args);
 	}
 
 	@Bean
-    public ApplicationRunner runner() {
-        return args -> {
-			dataImporter.readFile();
-        };
-    }
+	@Profile("console")
+	public ApplicationRunner runner(DataImporter dataImporter,
+			@Value("${aaejo.jds.data-importer.file}") String file,
+			@Value("${aaejo.jds.data-importer.password}") Optional<String> password) {
+		return args -> {
+			FileInputStream excelFile = new FileInputStream(new File(file));
+			dataImporter.readFile(excelFile, password);
+		};
+	}
 
 }
